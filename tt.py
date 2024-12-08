cccff7c487f67e36b908ba4d93332a6a207f28af
Hopanchuk Maksym <locksywork@gmail.com>
2024-12-08 20:24:24 +0200

    Add files via upload

---
+++ tt.py
@@ -0,0 +1,9 @@
+text = "hello world"
+name = "Максим"
+surname = "Гопанчук"
+age = 16
+
+print("Значення змінної text:", text, "| Тип:", type(text))
+print("Значення змінної name:", name, "| Тип:", type(name))
+print("Значення змінної surname:", surname, "| Тип:", type(surname))
+print("Значення змінної age:", age, "| Тип:", type(age))
