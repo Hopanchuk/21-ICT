3b5b940287f04f6bd7162edc1f0ffd0368a36c40
Hopanchuk Maksym <locksywork@gmail.com>
2024-12-29 20:41:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import telebot
+
+
+API_TOKEN = "8082904025:AAGFsnZ8zNNJ_Q_TYIdRiPOvVKdDz07kKJY"
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+if __name__ == "__main__":
+    print("Бота запущено!")
+    bot.infinity_polling()
+
+
